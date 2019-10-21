1ec46a29d86f00af5c604dc4f83dd5790c31ff94
dezhizhang <1541609448@qq.com>
2019-10-21 22:01:54 +0800

    数据类型

--- main.dart
+++ main.dart
@@ -1,7 +1,90 @@
+// main() {
+//    String str1 = 'hello';
+//    String str2 = 'world';
+//    String str = str1 + str2;
+//    print(str);
+
+// }
+
+// main() {
+//   double b = 10.12;
+//   print(b);
+
+// }
+
+// main() {
+//   double b = 12.0;
+//   double a = 12.49;
+//   double c = a + b;
+//   print(c);
+
+// }
+
+// main() {
+//   var list = ['a','b','c','d'];
+//   print(list);
+
+// }
+
+// main() {
+//   var list = ['a','b','c','d'];
+//   print(list);
+
+// }
+
 main() {
-   String str1 = 'hello';
-   String str2 = 'world';
-   String str = str1 + str2;
-   print(str);
+  // var list = new List();
+  // list.add('张三');
+  // list.add('李四');
+  // list.add('王五');
+  // print(list);
+  // var list = new List();
+  // list.add('相信他');
+  // list.add('哈哈');
+
+  // var list = new List<String>();
+  // list.add('1');
+  // list.add('2');
+  // print(list);
+
+  // var list = new List<String>();
+  // list.add('1');
+  // list.add('2');
+  // print(list);
+
+  // var person = {
+  //   'name':'张三',
+  //   'age':20
+  // };
+
+  // print(person['name']);
+
+  // var person = {
+  //   'name':'张三',
+  //   'age':20
+  // };
+
+  // print(person['name']);
+
+  // var person = {
+  //   'name':'张三',
+  //   'age':20
+  // };
+
+  // print(person['name']);
+
+  var person = {
+    'name':'张三',
+    'age':20
+  };
+
+  print(person['name']);
+  
+
+
+
+
+
+
 
 }
