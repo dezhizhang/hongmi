cc91cd066fb3900b8547cc15007d75c5f9501932
dezhizhang <1541609448@qq.com>
2019-10-23 21:01:49 +0800

    修改

--- main.dart
+++ main.dart
@@ -1,41 +1,10 @@
 main() {
+
+
   List myList = ['red','yellow','pink'];
-  // myList.addAll(['green','white']);
-  // print(myList.reversed);
-  // print(myList.remove('yellow'));
-  // print(myList);
-  // myList.insert(3, 'green');
-  // print(myList);
-
-  // Set set = new Set();
-  // set.add('red');
-  // set.add('red');
-  // set.add('green');
-  // print(set);
-
-  // Set set = new Set();
-  // set.add('red');
-  // set.add('green');
-  // set.toList();
-  // List list = set.toList();
-  // print(list);
-
-
-  // print(set.toList());
-
-  // Map map = new Map();
-  // map['name'] = '周华建';
-  // print(map);
-
-  // Map map = new Map();
-  // map['name'] = '张德志';
-  // print(map);
-
-  // Map map = new Map();
-  // map.addAll({
-  //   'work':['info']
-  // });
-  // print(map);
+  for(var i=0;i<myList.length;i++) {
+    print(myList[i]);
+  }
 
 
 
