a33ec7eb58a7800b4b24ae62e9d5335216a798ee
dezhizhang <1541609448@qq.com>
2019-10-23 20:40:35 +0800

    set

--- main.dart
+++ main.dart
@@ -4,8 +4,26 @@ main() {
   // print(myList.reversed);
   // print(myList.remove('yellow'));
   // print(myList);
-  myList.insert(3, 'green');
-  print(myList);
+  // myList.insert(3, 'green');
+  // print(myList);
+
+  // Set set = new Set();
+  // set.add('red');
+  // set.add('red');
+  // set.add('green');
+  // print(set);
+
+  Set set = new Set();
+  set.add('red');
+  set.add('green');
+  set.toList();
+  List list = set.toList();
+  print(list);
+  
+
+  // print(set.toList());
+
+
 
 
 
