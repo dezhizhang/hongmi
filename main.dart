0c759e761890d673fed77ec9aecd76793155dc9a
dezhizhang <1541609448@qq.com>
2019-10-24 20:45:31 +0800

    构造函数

--- main.dart
+++ main.dart
@@ -1,14 +1,18 @@
 class Person{
-  String name = '张三';
-  int age = 22;
+  String name;
+  int age;
+  Person(String name,int age) {
+    this.name = name;
+    this.age = age;
+  }
   getInfo() {
     return '${this.name} ----${this.age}';
   }
 }
 
 main() {
-  var p = new Person();
+  var p = new Person('周建建',10);
   print(p.getInfo());
-  
+
 }
 
