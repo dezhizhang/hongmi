b2d79f40f63935f6869d3879c096d7ab6de88937
dezhizhang <1541609448@qq.com>
2019-10-24 21:11:53 +0800

    实例化

--- main.dart
+++ main.dart
@@ -1,7 +1,24 @@
-import 'Animal.dart';
+// import 'Animal.dart';
+
+
+// void main() {
+//   var aminal = new Animal('哈哈', 12);
+//   print(aminal.printInfo());
+// }
+
+
+class Rect{
+  num height;
+  num width;
+  Rect(this.height,this.width);
+  area() {
+    return this.width * this.height;
+  }
+}
 
 
 void main() {
-  var aminal = new Animal('哈哈', 12);
-  print(aminal.printInfo());
+  Rect r = new Rect(10, 20);
+  print(r.area());
+  
 }
