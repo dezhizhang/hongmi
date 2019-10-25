1002e592c3ebfd1c089c6efef921b8601d3499c2
dezhizhang <1541609448@qq.com>
2019-10-25 23:52:24 +0800

    继承

--- Animal.dart
+++ Animal.dart
@@ -1,24 +1,32 @@
+// class Person{
+//   String name;
+//   int age;
+//   Person(this.name,this.age);
+//   printInfo() {
+//     print('${this.name} ----${this.age}');
+
+//   }
+
+// }
+
+// class Web extends Person{
+//   String sex;
+//     Web(String name,int age,String sex):super(name,age){
+//        this.sex = sex;
+//     }
+//     run() {
+//       print('${this.name} ---${this.age} ----${this.sex}');
+//     }
+
+// }
+
+// main() {
+//   var web = new Web('哈哈', 22,'男');
+//   web.printInfo();
+//   web.run();
+
+// }
+
 class Person{
-  String name;
-  int age;
-  Person(this.name,this.age);
-  printInfo() {
-    print('${this.name} ----${this.age}');
-
-  }
-
-}
-
-class Web extends Person{
-    Web(String name,int age):super(name,age){
-
-    }
-
-}
-
-main() {
-  var web = new Web('哈哈', 22);
-  web.printInfo();
   
-
 }
