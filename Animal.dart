f4b903a20900baa5ce76819fbd2fdf930c8e681e
dezhizhang <1541609448@qq.com>
2019-10-26 00:04:18 +0800

    修改

--- Animal.dart
+++ Animal.dart
@@ -27,6 +27,27 @@
 
 // }
 
-class Person{
-  
-}
+// class Person{
+//   String name;
+//   int age;
+//   Person(this.name,this.age);
+//   printInfo() {
+//     print('${this.name} ---${this.age}');
+//   }
+// }
+
+// class Web extends Person{
+//   String sex;
+//   Web(String name,int age,String sex):super(name,age){
+//     this.sex = sex;
+//   }
+//   run() {
+//     print('${this.name} ---${this.age} ----${this.sex}');
+// }
+// }
+
+// main() {
+//   var web = new Web('哈哈',22,'男');
+//   web.run();
+
+// }
