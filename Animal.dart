a4701cc94483b39ccebc752baee076496dcd4af9
dezhizhang <1541609448@qq.com>
2019-10-25 23:11:32 +0800

    提交

--- Animal.dart
+++ Animal.dart
@@ -1,10 +1,23 @@
-class Animal{
-  String _name;
-  int _age;
-  Animal(this._name,this._age);
+// class Animal{
+//   String _name;
+//   int _age;
+//   Animal(this._name,this._age);
 
-  printInfo(){
-    return '${this._name} ----${this._age}';
+//   printInfo(){
+//     return '${this._name} ----${this._age}';
 
+//   }
+// }
+
+class Person{
+  static String name = '张三';
+  static show () {
+    return 123;
   }
+
+}
+
+main() {
+ print(Person.show());
+
 }
